3e85251825ec76dc542cfa20206d2697d30d30c6
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-07 01:44:56 +0900

    implement nwords

--- wc.c
+++ wc.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 // printf("%2d %2d %2d %s\n",  3,  3,  10, ".gitignore");
@@ -8,11 +9,27 @@ int main(int argc, char **argv) {
   int nlines = 0;
   int nwords = 0;
   int nbytes = 0;
+  bool in_word = false;
 
   int c;
   while ((c = fgetc(stdin)) != EOF) {
     if (c == '\n')
       nlines++;
+
+    switch (c) {
+    case ' ':
+    case '\t':
+    case '\v':
+    case '\n':
+      in_word = false;
+      break;
+    default:
+      if (in_word == false) {
+        in_word = true;
+        nwords++;
+      }
+    }
+
     nbytes++;
   }
 
