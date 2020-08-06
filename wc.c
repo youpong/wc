81dffb9458d6c11643b37c838fa6950871e3aa29
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-07 01:25:57 +0900

    implement nlines

--- wc.c
+++ wc.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+
+// printf("%2d %2d %2d %s\n",  3,  3,  10, ".gitignore");
+// printf("%3d %3d %3d %s\n", 17, 32, 180, "Makefile");
+// printf("%3d %3d %3d %s\n", 20, 35, 190, "total");
+
 int main(int argc, char **argv) {
+  int nlines = 0;
+  int nwords = 0;
+  int nbytes = 0;
+
+  int c;
+  while ((c = fgetc(stdin)) != EOF) {
+    nbytes++;
+  }
+
+  printf("%7d %7d %7d\n", nlines, nwords, nbytes);
+
   return 0;
 }
