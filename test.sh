8ec2651154345312151a69f2493743c6d80ec8e9
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-08 00:44:28 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,13 @@ function error() {
     exit 1
 }
 
+# stdin
 cmp <(cat wc.c|./wc) <(cat wc.c|wc) || error "Error: $LINENO"
+
+# single file
 cmp <(./wc wc.c) <(wc wc.c) || error "Error: $LINENO"
-cmp <(./wc wc.c wc.c) <(wc wc.c wc.c) || error "Error: $LINENO"
+
+# multiple file
 cmp <(./wc wc.c Makefile) <(wc wc.c Makefile) || error "Error: $LINENO"
 
 echo "Ok." 
